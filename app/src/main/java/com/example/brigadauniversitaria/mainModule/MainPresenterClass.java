2ce7e8242e0d7e078dcd3ced28840e1a08fd09a7
yonat <yonathan.tovar@alumnos.udg.mx>
2021-08-23 21:53:27 -0500

    PRESENTER IMPLEMENT

---
+++ app/src/main/java/com/example/brigadauniversitaria/mainModule/MainPresenterClass.java
@@ -0,0 +1,121 @@
+package com.example.brigadauniversitaria.mainModule;
+
+import com.example.brigadauniversitaria.common.pojo.User;
+import com.example.brigadauniversitaria.mainModule.events.MainEvent;
+import com.example.brigadauniversitaria.mainModule.model.MainInteractor;
+import com.example.brigadauniversitaria.mainModule.model.MainInteractorClass;
+import com.example.brigadauniversitaria.mainModule.view.MainView;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+public class MainPresenterClass implements MainPresenter{
+    private MainView mView;
+    private MainInteractor mInteractor;
+
+    public MainPresenterClass(MainView mView) {
+        this.mView = mView;
+        this.mInteractor = new MainInteractorClass();
+    }
+
+    @Override
+    public void onCreate() {
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        EventBus.getDefault().unregister(this);
+        mView = null;
+    }
+
+    @Override
+    public void onPause() {
+        if (mView != null){
+            mInteractor.unsubcribeToUserList();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        if (mView != null){
+            mInteractor.subcribeToUserList();
+        }
+    }
+
+    @Override
+    public void singOff() {
+        mInteractor.unsubcribeToUserList();
+        mInteractor.singOff();
+        onDestroy();
+    }
+
+    @Override
+    public User getCurrentUser() {
+        return mInteractor.getCurrentUser();
+    }
+
+    @Override
+    public void removeFriend(String friendUid) {
+        if (mView != null){
+            mInteractor.removeFriend(friendUid);
+        }
+    }
+
+    @Override
+    public void acceptRequest(User user) {
+        if (mView != null){
+            mInteractor.acceptRequest(user);
+        }
+    }
+
+    @Override
+    public void denyRequest(User user) {
+        if (mView != null){
+            mInteractor.denyRequest(user);
+        }
+    }
+    @Subscribe
+    @Override
+    public void onEventListener(MainEvent event) {
+        if (mView != null){
+            User user = event.getUser();
+            switch (event.getTypeEvent()){
+                case MainEvent.USER_ADDED:
+                    mView.friendAdded(user);
+                    break;
+                case MainEvent.USER_UPDATE:
+                    mView.friendUpdate(user);
+                    break;
+                case MainEvent.USER_REMOVE:
+                    if (user != null) {
+                        mView.friendRemove(user);
+                    } else {
+                        mView.showFriendRemove();
+                    }
+                    break;
+                case MainEvent.REQUEST_ADDED:
+                    mView.requestAdded(user);
+                    break;
+                case MainEvent.REQUEST_UPDATE:
+                    mView.requestUpdate(user);
+                    break;
+                case MainEvent.REQUEST_REMOVE:
+                    mView.requestRemove(user);
+                    break;
+                case MainEvent.REQUEST_ACCEPTED:
+                    mView.showRequestAccepted(user.getUsername());
+                    break;
+                case MainEvent.REQUEST_DENIED:
+                    mView.showRequestDenied();
+                    break;
+                case MainEvent.ERROR_SERVER:
+                    mView.showError(event.getResMsg());
+                    break;
+
+
+
+            }
+        }
+    }
+}
